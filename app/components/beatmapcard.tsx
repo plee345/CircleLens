0cd4866a9ca2d70cc6559f3624c1e32223b4b129
plee345 <plee09902@gmail.com>
2026-09-16 01:44:57 -0400

    feat: add new card type

--- app/components/beatmapcard.tsx
+++ app/components/beatmapcard.tsx
@@ -1,3 +1,14 @@
+import { Badge } from "~/components/ui/badge"
+import { Button } from "~/components/ui/button"
+import {
+  Card,
+  CardAction,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card"
+
 interface ProfileAttributes {
   img: string;
   name: string;
@@ -11,24 +22,84 @@ interface ProfileAttributes {
   playTime: number;
   nation: string;
 }
+interface AdvancedStats {
+  xPP: number;
+  xRank: number;
+  xStarRating: number;
+}
+
+
+
 {
   /*option for horizontal cards alongside vertical cards?*/
 }
-export default function BeatmapCard(props: ProfileAttributes) {
+
+function BeatmapCardBuildDefault(profile: ProfileAttributes, adStats: AdvancedStats) {
   return (
     <div className="w-max rounded border-2 border-gray-900 bg-linear-65 from-pink-500 to-rose-600 text-center text-white shadow-xl/40 ring-4 ring-white">
       <h1 className="border-b-4 text-center">
-        {props.name} - {props.joinDate} - {props.nation}
+        {profile.name} - {profile.joinDate} - {profile.nation}
       </h1>
-      <img src={props.img} className="mx-auto flex" />
+      <img src={profile.img} className="mx-auto flex object-contain" />
       <h1 className="border-t-4">
-        #{props.rankGlobal} Global - #{props.rankNation} Country
+        #{profile.rankGlobal} Global - #{profile.rankNation} Country
       </h1>
       <h1>
-        {props.totalPP}pp {props.accuracy}% {props.playTime} Hours
+        {profile.totalPP}pp {profile.accuracy}% {profile.playTime} Hours
       </h1>
       <h1>
-        PlayCount: {props.playCount} {props.hitsPerPlay} Hits Per Play
+        PlayCount: {profile.playCount} {profile.hitsPerPlay} Hits Per Play
+      </h1>
+    </div>
+  );
+}
+
+export function PresetBigImage({/*profile: ProfileAttributes, adStats: AdvancedStats*/ }) {
+  return (
+    <Card className="relative w-[270px] max-w-full pt-0">
+      <div className="flex aspect-[45/32] w-full items-center justify-center bg-linear-65 from-pink-500 to-rose-600">
+        <img
+          src="https://media.tenor.com/ZLHdbFbs26sAAAAj/kirby-cute.gif"
+          alt="Kirby"
+          className="h-1/2 w-1/2 object-contain"
+        />
+      </div>
+      <CardHeader>
+        <CardAction>
+          <Badge variant="secondary">Featured</Badge>
+        </CardAction>
+        <CardTitle>plee</CardTitle>
+        <CardDescription>
+          A practical talk on component APIs, accessibility, and shipping
+          faster.
+        </CardDescription>
+      </CardHeader>
+      <CardFooter>
+        <Button className="w-full">View Event</Button>
+      </CardFooter>
+    </Card>
+  );
+}
+
+function BeatmapCardConstructor(profile: ProfileAttributes, adStats: AdvancedStats) {
+
+}
+
+export default function BeatmapCard(profile: ProfileAttributes) {
+  return (
+    <div className="w-max rounded border-2 border-gray-900 bg-linear-65 from-pink-500 to-rose-600 text-center text-white shadow-xl/40 ring-4 ring-white">
+      <h1 className="border-b-4 text-center">
+        {profile.name} - {profile.joinDate} - {profile.nation}
+      </h1>
+      <img src={profile.img} className="mx-auto flex" />
+      <h1 className="border-t-4">
+        #{profile.rankGlobal} Global - #{profile.rankNation} Country
+      </h1>
+      <h1>
+        {profile.totalPP}pp {profile.accuracy}% {profile.playTime} Hours
+      </h1>
+      <h1>
+        PlayCount: {profile.playCount} {profile.hitsPerPlay} Hits Per Play
       </h1>
     </div>
   );
--- app/routes/profile.tsx
+++ app/routes/profile.tsx
@@ -1,26 +1,148 @@
 import type { Route } from "./+types/dailyrotation";
 import Navbar from "~/components/navbar";
 import Footer from "~/components/footer";
-import BeatmapCard from "~/components/beatmapcard";
+import BeatmapCard, { PresetBigImage } from "~/components/beatmapcard";
+import { Checkbox } from "~/components/ui/checkbox"
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+  FieldLegend,
+  FieldSet,
+} from "~/components/ui/field"
 
-export default function DailyRotation() {
+export function CheckboxGroup() {
   return (
-    <>
+    <FieldSet>
+      <FieldLegend variant="label">
+        Statistic Options:
+      </FieldLegend>
+      <FieldDescription>
+        Modify the attributes listed to change the stats listed on the card.
+      </FieldDescription>
+      <FieldGroup className="gap-3">
+        <Field orientation="responsive">
+          <Checkbox
+            id="finder-pref-9k2-hard-disks-ljj-checkbox"
+            name="finder-pref-9k2-hard-disks-ljj-checkbox"
+            defaultChecked
+          />
+          <FieldLabel
+            htmlFor="finder-pref-9k2-hard-disks-ljj-checkbox"
+            className="font-normal"
+          >
+            Global Rank
+          </FieldLabel>
+          <Checkbox
+            id="finder-pref-9k2-hard-disks-ljj-checkbox"
+            name="finder-pref-9k2-hard-disks-ljj-checkbox"
+            defaultChecked
+          />
+          <FieldLabel
+            htmlFor="finder-pref-9k2-hard-disks-ljj-checkbox"
+            className="font-normal"
+          >
+            Global Rank
+          </FieldLabel>
+        </Field>
+        <Field orientation="responsive">
+          <Checkbox
+            id="finder-pref-9k2-external-disks-1yg-checkbox"
+            name="finder-pref-9k2-external-disks-1yg-checkbox"
+            defaultChecked
+          />
+          <FieldLabel
+            htmlFor="finder-pref-9k2-external-disks-1yg-checkbox"
+            className="font-normal"
+          >
+            Country Rank
+          </FieldLabel>
+        </Field>
+        <Field orientation="horizontal">
+          <Checkbox
+            id="finder-pref-9k2-cds-dvds-fzt-checkbox"
+            name="finder-pref-9k2-cds-dvds-fzt-checkbox"
+          />
+          <FieldLabel
+            htmlFor="finder-pref-9k2-cds-dvds-fzt-checkbox"
+            className="font-normal"
+          >
+            Performance Points
+          </FieldLabel>
+        </Field>
+        <Field orientation="horizontal">
+          <Checkbox
+            id="finder-pref-9k2-connected-servers-6l2-checkbox"
+            name="finder-pref-9k2-connected-servers-6l2-checkbox"
+          />
+          <FieldLabel
+            htmlFor="finder-pref-9k2-connected-servers-6l2-checkbox"
+            className="font-normal"
+          >
+            Accuracy
+          </FieldLabel>
+        </Field>
+        <Field orientation="horizontal">
+          <Checkbox
+            id="finder-pref-9k2-connected-servers-6l2-checkbox"
+            name="finder-pref-9k2-connected-servers-6l2-checkbox"
+          />
+          <FieldLabel
+            htmlFor="finder-pref-9k2-connected-servers-6l2-checkbox"
+            className="font-normal"
+          >
+            Hours Played
+          </FieldLabel>
+        </Field>
+        <Field orientation="horizontal">
+          <Checkbox
+            id="finder-pref-9k2-connected-servers-6l2-checkbox"
+            name="finder-pref-9k2-connected-servers-6l2-checkbox"
+          />
+          <FieldLabel
+            htmlFor="finder-pref-9k2-connected-servers-6l2-checkbox"
+            className="font-normal"
+          >
+            PlayCount
+          </FieldLabel>
+        </Field>
+      </FieldGroup>
+
+    </FieldSet>
+  )
+}
+
+
+
+export default function Profile() {
+  return (
+    <div className="flex min-h-screen flex-col">
       <Navbar />
-      <BeatmapCard
-        img="https://media.tenor.com/ZLHdbFbs26sAAAAj/kirby-cute.gif"
-        name="plee"
-        joinDate="May 2018"
-        nation="United States"
-        rankGlobal={872}
-        rankNation={189}
-        totalPP={14107}
-        accuracy={98.71}
-        playCount={51505}
-        hitsPerPlay={323}
-        playTime={971}
-      />
+      <div className="mx-auto grid w-full max-w-6xl grid-cols-6 gap-4 px-4 lg:grid-cols-[270px_minmax(0,1fr)]">
+        <div className="min-w-0">
+          <BeatmapCard
+            img="https://media.tenor.com/ZLHdbFbs26sAAAAj/kirby-cute.gif"
+            name="plee"
+            joinDate="May 2018"
+            nation="United States"
+            rankGlobal={872}
+            rankNation={189}
+            totalPP={14107}
+            accuracy={98.71}
+            playCount={51505}
+            hitsPerPlay={323}
+            playTime={971}
+          />
+        </div>
+        <div className="min-w-0">
+          <CheckboxGroup />
+        </div>
+        <div className="min-w-0">
+          <PresetBigImage />
+        </div>
+      </div>
       <Footer />
-    </>
+    </div>
   );
 }
